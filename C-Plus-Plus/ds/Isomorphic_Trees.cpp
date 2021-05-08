b2fa5d45e6e5ba3fb6dac5373eaab69d1f00e072
ddarshney <68664921+ddarshney@users.noreply.github.com>
2021-05-08 16:55:14 +0530

    Update Isomorphic_Trees.cpp

--- C-Plus-Plus/ds/Isomorphic_Trees.cpp
+++ C-Plus-Plus/ds/Isomorphic_Trees.cpp
@@ -21,12 +21,32 @@ class BinaryTreeNode {
        		delete right;
     }
 };
+The first line of each test case will contain the values of the nodes of the first tree in the level order form ( -1 for NULL node).
 
-bool checkIsomorphism(BinaryTreeNode<int>* n1, BinaryTreeNode<int>* n2){
-	/*Write your code here. 
-	*Don't write main().
-	*Don't take input, it is passed as function argument.
-	*Don't print output.
-	*Taking input and printing output is handled automatically.
-	*/ 
+The second line of each test case will contain the values of the nodes of the second tree in the level order form ( -1 for NULL node). 
+bool isIsomorphic(BinaryTreeNode<int> *t1, BinaryTreeNode<int> *t2) {
+    if (t1 == NULL && t2 == NULL) {
+        return true;
+    }
+
+    else if (t1 == NULL || t2 == NULL) {
+        return false;
+    }
+
+    if (t1->data != t2->data) {
+        return false;
+    }
+
+    return ((isIsomorphic(t1->left, t2->left) && isIsomorphic(t1->right, t2->right)) ||
+            (isIsomorphic(t1->left, t2->right) && isIsomorphic(t1->right, t2->left)));
 }
+
+
+bool checkTree(BinaryTreeNode<int> *tree1, BinaryTreeNode<int> *tree2) {
+    return isIsomorphic(tree1, tree2);
+}
+/*
+        Time Complexity : O(min(m,n))
+        Space Complexity : O(min(m,n)),
+        where m and n are the number of non-NULL nodes in the first and second tree respectively
+ */
